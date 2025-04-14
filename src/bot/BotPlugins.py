fb9c4e49f2f21ff7590b956c7294dbf19a53ecff
Ca-tt <damir96.lukyanenko@gmail.com>
2025-04-14 17:05:34 +0300

    tested things out

--- src/bot/BotPlugins.py
+++ src/bot/BotPlugins.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Callable, List
 
 #? engine
 from bot_engine.languages.Languages import Languages
@@ -13,27 +14,38 @@ from src.data.locales.ru import RU_LOCALE
 #? modules
 from src.dialogs.AdminDialogs import AdminDialogs
 
+
 @dataclass
 class BotPlugins:
 
     #? bot dependencies
     def setup_database(self):
+        """ Connects MongoDB, prepares cache """
         Database()
 
 
     def setup_languages(self):
-        Languages(active_lang="uk")
+        """ setup locales and active language """
+        languages = Languages()
+
+        languages.add_locale(UK_LOCALE)
+        languages.add_locale(RU_LOCALE)
+
+        languages.active_lang = "uk"
 
 
     def setup_dialog_generator(self):
+        """ Prepares DialogGenerator for work """
         pass
 
 
     def setup_dialogs(self):
+        """ Prepares all bot dialogs """
         pass
 
 
-    def setup_plugins(self):
+    def setup_plugins(self) -> List[Callable]:
+        """ returns a list of all bot plugin functions """
         return [
             self.setup_database,
             self.setup_languages,
--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ from bot_engine.bot.Bot import Bot
 from src.bot.BotPlugins import BotPlugins
 
 
+#! 1) Следующий шаг: задать команду /start боту в левом меню (languages + DialogGenerator or a new class MenuCommands.set_menu_commands) 
+#! 2) И, конечно же, задать команду /start боту, использую генератор диалогов DialogGenerator
+
 bot = Bot()
 plugins = BotPlugins().setup_plugins()
 
